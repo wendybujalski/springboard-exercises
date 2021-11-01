62ccd1056e972d30daf9921272e2992f7d157cad
wendybujalski <simonisnailtoe@gmail.com>
2021-11-01 14:58:22 -0400

    DOM manipulation exercise

---
+++ 4.2.17/app.js
@@ -0,0 +1,41 @@
+// 1
+//let container = document.getElementById("container");
+
+// 2
+//container = document.querySelector("#container");
+
+// 3
+//let listItemsSecond = document.querySelectorAll(".second");
+
+// 4
+//let listItemOlSecond = document.querySelector("ol .third");
+
+// 5
+//container.innerText = "Hello!";
+
+// 6
+//let footer = document.querySelector("div.footer");
+//footer.classList.add("main");
+
+// 7
+//footer.classList.remove("main");
+
+// 8
+//let li = document.createElement("li");
+
+// 9
+//li.innerText = "four";
+
+// 10
+//let ul = document.querySelector("ul");
+//ul.appendChild(li);
+
+// 11
+//let orderedLi = document.querySelectorAll("ol li");
+
+//for(let i = 0; i < orderedLi.length; i++) {
+//    orderedLi[i].style.backgroundColor = "green";
+//}
+
+// 12
+//footer.remove();
