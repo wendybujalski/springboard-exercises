02eeff9d9649acd8adc4991d157002ed89b9e603
wendybujalski <simonisnailtoe@gmail.com>
2021-11-12 13:17:10 -0500

    10.3.6 arrow functions

---
+++ 10.3.6/app.js
@@ -0,0 +1,5 @@
+// Map Callback
+const doubleFunction = arr => arr.map(val => val * 2);
+
+// All Arrow Functions
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
